0c73b4751b354a9b29f48894b311f7350920e098
Luigi Thirty <luigi30@gmail.com>
2017-07-19 23:44:57 -0400

    Initial Commit

---
+++ FruitMachine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FruitMachine
+//
+//  Created by Christopher Rohl on 7/19/17.
+//  Copyright © 2017 Christopher Rohl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FruitMachine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FruitMachine
+//
+//  Created by Christopher Rohl on 7/19/17.
+//  Copyright © 2017 Christopher Rohl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
